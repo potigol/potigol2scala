1202cf5f024d72e79dee40679da1c8ac98c8248e
Leonardo Lucena <leonardo.lucena@escolar.ifrn.edu.br>
2017-04-30 21:55:56 -0300

    Update potigol2scala.sh

--- potigol2scala.sh
+++ potigol2scala.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -li
 
-cp potigolutil.scala $1.scala
+echo "/* Traducao de Potigol para Scala usando http://github.com/potigol/potigol2scala" > $1.scala
+cat $1 >> $1.scala
+echo "*/" >> $1.scala
+cat potigolutil.scala >> $1.scala
 echo "object Main extends App{" >> $1.scala
 potigol -d $1 | tail -n +6 >> $1.scala
 echo "}" >> $1.scala
